5a3d24b05978f0975f8dcfbedca86d369cbe586b
Sunny Singh <sunnysinghrajavatssr@gmail.com>
2019-07-27 16:43:32 +0530

    removed codemirror

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ let jsMethods = [{
   protoOf: "Array",
   mutates: false,
   description: "The map() method creates a new array with the results of calling a provided function on every element in the calling array.",
-  syntax: "arr.lastIndexOf(searchElement[, fromIndex])",
+  syntax: "var new_array = arr.map(function callback(currentValue[, index[, array]]) { \n\
+    // Return element for new_array \n\
+}[, thisArg])",
   mdnLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map",
   example: " \
   var array1 = [1, 4, 9, 16]; \n\
@@ -46,10 +48,10 @@ let showMethods = (methods) => {
                   <h5 class="method-heading">Description</h5>
                   <p class="description">${method.description}</p>
                   <h5 class="method-heading">Syntax</h5>
-                  <pre class="language-js"><code class="language-js">${method.syntax}</code></pre>
+                  <pre><code class="language-js">${method.syntax}</code></pre>
                   <a class="mdn-link" href="${method.mdnLink}" target="_blank">${method.mdnLink}</a>
                   <h5 class="method-heading">Example</h5>
-                  <pre class="language-js"><code class="language-js" >${method.example}</code></pre>
+                  <pre><code class="language-js" >${method.example}</code></pre>
                 </div>`
   });
 
